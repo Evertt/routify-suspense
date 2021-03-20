f7951acfe2c947af3bd687b4e0027a0ce91e3d18
Evert van Brussel <ecvanbrussel@gmail.com>
2021-03-20 14:30:52 +0100

    Make bug a choice

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -5,16 +5,20 @@
 
   const params = new URLSearchParams(window.location.search)
   let timeout = params.get('timeout') || 1500
+  let produceBug = parseInt(params.get('with-bug') ?? "1")
 
-  setTimeout(() => $ready(), window.routify.inBrowser ? timeout / 4 : timeout)
+  setTimeout(() => $ready(), window.routify.inBrowser && produceBug ? timeout / 4 : timeout)
 </script>
 
 <div class="outer">
   <div>Choose an amount of time to wait for SSR.</div>
-  <div>
+  <div class="right">
     <input type="range" bind:value={timeout} min=0 max=2100 step=100 />
-    {timeout} ms
-    <button on:click={_ => window.location = `?timeout=${timeout}`}>(Re)load</button>
+    <span>{timeout} ms</span>
+    <div>
+      <button on:click={_ => window.location = `?timeout=${timeout}`}>(Re)load with flash-bug</button><br />
+      <button on:click={_ => window.location = `?timeout=${timeout}&with-bug=0`}>(Re)load without flash-bug</button>
+    </div>
   </div>
 </div>
 
@@ -24,7 +28,15 @@
 
 <style>
   .outer {
-    @apply flex justify-between items-center my-4 px-2;
+    @apply flex justify-between items-center my-2 px-2;
+  }
+
+  .right {
+    @apply flex items-center;
+  }
+
+  .right > * {
+    @apply ml-4;
   }
 
   input, button {
